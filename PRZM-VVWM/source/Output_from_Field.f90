87338157b576c07268cfbdc9e93f4790cdf93168
Young, Dirk <young.dirk@epa.gov>
2023-09-19 20:41:04 -0400

    output file for field naming repair

--- PRZM-VVWM/source/Output_from_Field.f90
+++ PRZM-VVWM/source/Output_from_Field.f90
@@ -129,8 +129,8 @@ Module Output_From_Field
 	if (is_timeseriesfile .eqv. .FALSE. ) return  !if time series file is not specified, then leave
 	
     if (First_time_through_PRZM) then 
-		filename = full_run_identification // ".out"  !field output name    
-	    OPEN(Unit=TimeSeriesUnit2,FILE=trim(adjustl(filename)), STATUS='UNKNOWN') 
+		filename = trim(full_run_identification) // ".out"  !field output name    
+	    OPEN(Unit=TimeSeriesUnit2,FILE=filename, STATUS='UNKNOWN') 
         call  write_outputfile_header_2
         First_time_through_PRZM = .FALSE.
     end if
